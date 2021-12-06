4a49a7615dce2a4260835da444d898a1ccdaae13
EvgeniiRomanov <e78rus@mail.ru>
2021-12-06 07:17:52 +0300

    ПЗ 4

--- Занятие4/Домашние_задания/task1_2/main.py
+++ Занятие4/Домашние_задания/task1_2/main.py
@@ -1,5 +1,18 @@
-pattern = """(.|\n)+?"""
+import re
+
+
+#pattern = """(.|\n)+?"""
+pattern = r'^\"{3}.+?\"{3}'
+
+def task():
+    book_pattern = re.compile(pattern, re.DOTALL)  # флаг re.DOTALL описывает, что под символом точка может содержаться символ переноса строки
+
+    with open("input.txt", 'r', encoding="utf-8") as f:
+        for book in book_pattern.finditer(f.read()):
+            print(book.group())
 
 if __name__ == "__main__":
     # Write your solution here
-    pass
+    task()
+    print("------------------")
+    task()
