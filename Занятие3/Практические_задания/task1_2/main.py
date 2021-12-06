e5901b98b439ba8c15c8fd1f83d3b4076b4555ee
EvgeniiRomanov <e78rus@mail.ru>
2021-12-07 00:02:58 +0300

    ПЗ 3

--- Занятие3/Практические_задания/task1_2/main.py
+++ Занятие3/Практические_задания/task1_2/main.py
@@ -1,10 +1,12 @@
 def task():
-    list_words = ["Один", "Два", "Три"]
+    list_words = ["Один", "Два", "Три", "четыре", "Шарик ты балбес"]
 
     filename = "output.txt"
     with open(filename, "w", encoding="utf-8") as f:
         for word in list_words:
              # TODO с помощью метода write запишите построчно содержимое списка
+            #f.writelines(f'{word}\n')
+            #f.write(word+"\n")
             f.write(f'{word}\n')
     with open(filename, "r", encoding="utf-8") as f:
         for line in f:
