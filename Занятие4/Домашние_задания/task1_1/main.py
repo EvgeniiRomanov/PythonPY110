8eec5b9fee9fb2e61095abf46f53daee023de701
EvgeniiRomanov <e78rus@mail.ru>
2021-12-18 18:16:03 +0000

    ПЗ 4 - task_1

--- Занятие4/Домашние_задания/task1_1/main.py
+++ Занятие4/Домашние_задания/task1_1/main.py
@@ -7,7 +7,15 @@ ip4 = re.compile(r"""
           (?:25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])
 """, re.VERBOSE)
 
+def task():
+    with open("input_ip.txt", 'r', encoding="UTF8") as f:
+        #
+        # for line in f:
+        #     print(re.findall(ip4, line))
+
+        for line in ip4.finditer(f.read()):
+            print(line.group())
 
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    task()
+
